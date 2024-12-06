3dc8c085b8327d213912decc4f6147f0bb43128d
ramosmanel <emanuelramos20004@gmail.com>
2024-12-06 12:57:05 -0300

    feat: api structure created

---
+++ src/main/java/com/ms/email/dtos/EmailDTO.java
@@ -0,0 +1,25 @@
+package com.ms.email.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailDTO(
+        @NotBlank
+        String ownerRef,
+
+        @NotBlank
+        @Email
+        String emailFrom,
+
+        @NotBlank
+        @Email
+        String emailTo,
+
+        @NotBlank
+        String subject,
+
+        @NotBlank
+        String text
+
+) {
+}
