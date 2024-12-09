6fb89b4fa5d06572e8efecd803c55bcac02133da
ramosmanel <emanuelramos20004@gmail.com>
2024-12-09 10:23:16 -0300

    feat: adding first route

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -1,12 +1,37 @@
 package com.ms.email.controllers;
 
+import com.ms.email.dtos.EmailDTO;
+import com.ms.email.models.EmailModel;
 import com.ms.email.services.EmailService;
+import jakarta.validation.Valid;
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.InvocationTargetException;
 
 @RestController
 public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<?> sendingEmail(@RequestBody @Valid EmailDTO emailDTO) {
+        try {
+            EmailModel emailModel = new EmailModel();
+            BeanUtils.copyProperties(emailDTO, emailModel);
+            emailService.sendEmail(emailModel);
+            return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+        } catch (InvocationTargetException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (IllegalAccessException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
 }
